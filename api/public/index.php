ec2eb8a03df030ec5ae8f5c7625e83f5f4c3659f
Russkazkin <russkazkin@gmail.com>
2023-02-05 07:21:37 +0300

    Use Psr Http classes for HomeAction

--- api/public/index.php
+++ api/public/index.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 use App\Http\Action\HomeAction;
 use DI\ContainerBuilder;
+use Psr\Http\Message\ResponseFactoryInterface;
 use Slim\Factory\AppFactory;
+use Slim\Psr7\Factory\ResponseFactory;
 
 http_response_code(500);
 
@@ -16,6 +18,7 @@ $builder->addDefinitions([
     'config' => [
         'debug' => (bool) getenv('APP_DEBUG'),
     ],
+    ResponseFactoryInterface::class => Di\get(ResponseFactory::class),
 ]);
 
 $container = $builder->build();
--- api/src/Http/Action/HomeAction.php
+++ api/src/Http/Action/HomeAction.php
@@ -6,17 +6,27 @@ namespace App\Http\Action;
 
 use App\Http;
 use JsonException;
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseFactoryInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
 use stdClass;
 
-class HomeAction
+class HomeAction implements RequestHandlerInterface
 {
+    private ResponseFactoryInterface $factory;
+
+    public function __construct(ResponseFactoryInterface $factory)
+    {
+        $this->factory = $factory;
+    }
+
     /**
      * @throws JsonException
      */
-    public function __invoke(Request $request, Response $response, $args): Response
+    public function handle(ServerRequestInterface $request): ResponseInterface
     {
+        $response = $this->factory->createResponse();
         return Http::json($response, new stdClass());
     }
 }
