f043f73060e353bf85b6e5f9e6d10fcd4dbea03a
Russkazkin <russkazkin@gmail.com>
2023-02-05 08:03:25 +0300

    Extract container and dependencies

--- api/public/index.php
+++ api/public/index.php
@@ -2,26 +2,15 @@
 
 declare(strict_types=1);
 
-use App\Http\Action\HomeAction;
-use DI\ContainerBuilder;
-use Psr\Http\Message\ResponseFactoryInterface;
+use Psr\Container\ContainerInterface;
 use Slim\Factory\AppFactory;
-use Slim\Psr7\Factory\ResponseFactory;
 
 http_response_code(500);
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$builder = new ContainerBuilder();
-
-$builder->addDefinitions([
-    'config' => [
-        'debug' => (bool) getenv('APP_DEBUG'),
-    ],
-    ResponseFactoryInterface::class => Di\get(ResponseFactory::class),
-]);
-
-$container = $builder->build();
+/** @var ContainerInterface $container */
+$container = require __DIR__ . '/../config/container.php';
 
 $app = AppFactory::createFromContainer($container);
 
