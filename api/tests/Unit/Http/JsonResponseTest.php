796a4edcfeed2888110427987e49bb3fd65be35b
Russkazkin <russkazkin@gmail.com>
2023-09-16 03:49:28 +0300

    Make JsonResponseTest parameterized

--- api/tests/Unit/Http/JsonResponseTest.php
+++ api/tests/Unit/Http/JsonResponseTest.php
@@ -17,59 +17,41 @@ class JsonResponseTest extends TestCase
         self::assertEquals(201, $response->getStatusCode());
     }
 
-    public function testNull(): void
+    /**
+     * @dataProvider getCases
+     * @throws JsonException
+     */
+    public function testResponse(mixed $source, mixed $expect): void
     {
-        $response = new JsonResponse(null);
-
-        self::assertEquals('null', $response->getBody()->getContents());
+        $response = new JsonResponse($source);
+        self::assertEquals('application/json', $response->getHeaderLine('Content-Type'));
+        self::assertEquals($expect, $response->getBody()->getContents());
         self::assertEquals(200, $response->getStatusCode());
     }
 
     /**
-     * @throws JsonException
+     * @return array
      */
-    public function testInt(): void
-    {
-        $response = new JsonResponse(12);
-
-        self::assertEquals('12', $response->getBody()->getContents());
-        self::assertEquals(200, $response->getStatusCode());
-    }
-
-    public function testString(): void
-    {
-        $response = new JsonResponse('12');
-
-        self::assertEquals('"12"', $response->getBody()->getContents());
-        self::assertEquals(200, $response->getStatusCode());
-    }
-
-    /**
-     * @throws JsonException
-     */
-    public function testObject(): void
+    public static function getCases(): array
     {
         $object = new stdClass();
         $object->str = 'value';
         $object->int = 1;
         $object->none = null;
 
-        $response = new JsonResponse($object);
+        $array = [
+            'str' => 'value',
+            'int' => 1,
+            'none' => null
+        ];
 
-        self::assertEquals('{"str":"value","int":1,"none":null}', $response->getBody()->getContents());
-        self::assertEquals(200, $response->getStatusCode());
-    }
-
-    /**
-     * @throws JsonException
-     */
-    public function testArray(): void
-    {
-        $array = ['str' => 'value', 'int' => 1, 'none' => null];
-
-        $response = new JsonResponse($array);
-
-        self::assertEquals('{"str":"value","int":1,"none":null}', $response->getBody()->getContents());
-        self::assertEquals(200, $response->getStatusCode());
+        return [
+            'null' => [null, 'null'],
+            'empty' => ['', '""'],
+            'number' => [12, '12'],
+            'string' => ['12', '"12"'],
+            'object' => [$object, '{"str":"value","int":1,"none":null}'],
+            'array' => [$array, '{"str":"value","int":1,"none":null}'],
+        ];
     }
 }
