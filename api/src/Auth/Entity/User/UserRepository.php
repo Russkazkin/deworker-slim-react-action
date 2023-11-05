a3eb7babe26362c9a93b1e3a220cb060c66e54c0
Russkazkin <russkazkin@gmail.com>
2023-11-05 05:18:19 +0300

    Add password reset request

--- api/src/Auth/Entity/User/UserRepository.php
+++ api/src/Auth/Entity/User/UserRepository.php
@@ -18,4 +18,10 @@ interface UserRepository
      * @throws DomainException
      */
     public function get(Id $id): User;
+    /**
+     * @param Email $email
+     * @return User
+     * @throws DomainException
+     */
+    public function getByEmail(Email $email): User;
 }
