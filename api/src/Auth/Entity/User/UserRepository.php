ecf26d109e95ea1187cabb2657c344b6d45e8ebd
Russkazkin <russkazkin@gmail.com>
2023-12-09 15:16:20 +0300

    Add user remove logic

--- api/src/Auth/Entity/User/UserRepository.php
+++ api/src/Auth/Entity/User/UserRepository.php
@@ -11,6 +11,7 @@ interface UserRepository
     public function hasByEmail(Email $email): bool;
     public function hasByNetwork(NetworkIdentity $identity): bool;
     public function add(User $user): void;
+    public function remove(User $user): void;
     public function findByJoinConfirmToken(string $token): ?User;
     public function findByPasswordResetToken(string $token): ?User;
     public function findByNewEmailToken(string $token): ?User;
