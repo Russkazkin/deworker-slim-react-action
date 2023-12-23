a7c84221073792c7771d73a48c328cb000c75d1e
Russkazkin <russkazkin@gmail.com>
2023-12-23 11:01:15 +0300

    Fix psalm errors

--- api/src/Auth/Entity/User/UserRepository.php
+++ api/src/Auth/Entity/User/UserRepository.php
@@ -4,27 +4,94 @@ declare(strict_types=1);
 
 namespace App\Auth\Entity\User;
 
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\NonUniqueResultException;
+use Doctrine\ORM\NoResultException;
 use DomainException;
 
-interface UserRepository
+class UserRepository
 {
-    public function hasByEmail(Email $email): bool;
-    public function hasByNetwork(Network $network): bool;
-    public function add(User $user): void;
-    public function remove(User $user): void;
-    public function findByJoinConfirmToken(string $token): ?User;
-    public function findByPasswordResetToken(string $token): ?User;
-    public function findByNewEmailToken(string $token): ?User;
+    private EntityRepository $repo;
+    private EntityManagerInterface $em;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $repo = $em->getRepository(User::class);
+        $this->repo = $repo;
+        $this->em = $em;
+    }
+
     /**
-     * @param Id $id
-     * @return User
-     * @throws DomainException
+     * @throws NonUniqueResultException
+     * @throws NoResultException
      */
-    public function get(Id $id): User;
+    public function hasByEmail(Email $email): bool
+    {
+        return $this->repo->createQueryBuilder('t')
+                ->select('COUNT(t.id)')
+                ->andWhere('t.email = :email')
+                ->setParameter(':email', $email->getValue())
+                ->getQuery()->getSingleScalarResult() > 0;
+    }
+
     /**
-     * @param Email $email
-     * @return User
-     * @throws DomainException
+     * @throws NonUniqueResultException
+     * @throws NoResultException
      */
-    public function getByEmail(Email $email): User;
+    public function hasByNetwork(Network $network): bool
+    {
+        return $this->repo->createQueryBuilder('t')
+                ->select('COUNT(t.id)')
+                ->innerJoin('t.networks', 'n')
+                ->andWhere('n.network.name = :name and n.network.identity = :identity')
+                ->setParameter(':name', $network->getName())
+                ->setParameter(':identity', $network->getIdentity())
+                ->getQuery()->getSingleScalarResult() > 0;
+    }
+
+    public function findByJoinConfirmToken(string $token): User|null
+    {
+        /** @psalm-var User|null */
+        return $this->repo->findOneBy(['joinConfirmToken.value' => $token]);
+    }
+
+    public function findByPasswordResetToken(string $token): ?User
+    {
+        /** @psalm-var User|null */
+        return $this->repo->findOneBy(['passwordResetToken.value' => $token]);
+    }
+
+    public function findByNewEmailToken(string $token): ?User
+    {
+        /** @psalm-var User|null */
+        return $this->repo->findOneBy(['newEmailToken.value' => $token]);
+    }
+
+    public function get(Id $id): User
+    {
+        if (!$user = $this->repo->find($id->getValue())) {
+            throw new DomainException('User is not found.');
+        }
+        /** @var User $user */
+        return $user;
+    }
+    public function getByEmail(Email $email): User
+    {
+        if (!$user = $this->repo->findOneBy(['email' => $email->getValue()])) {
+            throw new DomainException('User is not found.');
+        }
+        /** @var User $user */
+        return $user;
+    }
+
+    public function add(User $user): void
+    {
+        $this->em->persist($user);
+    }
+
+    public function remove(User $user): void
+    {
+        $this->em->remove($user);
+    }
 }
