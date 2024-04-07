fa91ec8b9868cfbf924607ada842edb3ac7f6126
Russkazkin <russkazkin@gmail.com>
2024-04-07 09:44:29 +0300

    Migrate to twig

--- api/src/Auth/Service/JoinConfirmationSender.php
+++ api/src/Auth/Service/JoinConfirmationSender.php
@@ -10,20 +10,32 @@ use App\Frontend\FrontendUrlGenerator;
 use RuntimeException;
 use Swift_Mailer;
 use Swift_Message;
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
 
 class JoinConfirmationSender
 {
-    public function __construct(private readonly Swift_Mailer $mailer, private readonly FrontendUrlGenerator $frontend)
-    {
+    public function __construct(
+        private readonly Swift_Mailer $mailer,
+        private readonly FrontendUrlGenerator $frontend,
+        private Environment $twig,
+    ) {
     }
 
+    /**
+     * @throws SyntaxError
+     * @throws RuntimeError
+     * @throws LoaderError
+     */
     public function send(Email $email, Token $token): void
     {
         $message = (new Swift_Message('Join Confirmation'))
             ->setTo($email->getValue())
-            ->setBody($this->frontend->generate('join/confirm', [
-                    'token' => $token->getValue(),
-                ]));
+            ->setBody($this->twig->render('auth/join/confirm.html.twig', [
+                'url' => $this->frontend->generate('join/confirm', ['token' => $token->getValue()]),
+            ]), 'text/html');
 
         if ($this->mailer->send($message) === 0) {
             throw new RuntimeException('Unable to send email.');
