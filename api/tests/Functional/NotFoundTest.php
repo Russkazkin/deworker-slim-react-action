a5c188c5a2211e30c2e9df8d184d14e8e3802f92
skazkin <russkazkin@gmail.com>
2024-05-19 06:48:27 +0300

    Improve not found page test

--- api/tests/Functional/NotFoundTest.php
+++ api/tests/Functional/NotFoundTest.php
@@ -4,12 +4,30 @@ declare(strict_types=1);
 
 namespace Test\Functional;
 
+use DMS\PHPUnitExtensions\ArraySubset\ArraySubsetAsserts;
+use Exception;
+use JsonException;
+
 class NotFoundTest extends WebTestCase
 {
+    use ArraySubsetAsserts;
+
+    /**
+     * @throws JsonException
+     * @throws Exception
+     */
     public function testNotFound(): void
     {
         $response = $this->app()->handle(self::json('GET', '/not-found'));
 
         self::assertEquals(404, $response->getStatusCode());
+        self::assertJson($body = (string)$response->getBody());
+
+        /** @var array $data */
+        $data = json_decode($body, true, 512, JSON_THROW_ON_ERROR);
+
+        self::assertArraySubset([
+            'message' => '404 Not Found',
+        ], $data);
     }
 }
