2dfc0b4d6f5c9d5b3dcb21180437892ab95457ca
skazkin <russkazkin@gmail.com>
2024-06-01 04:46:06 +0300

    Add Json helper class

---
+++ api/tests/Functional/Json.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Test\Functional;
+
+use JsonException;
+
+class Json
+{
+    /**
+     * @throws JsonException
+     */
+    public static function decode(string $data): array
+    {
+        return json_decode($data, true, 512, JSON_THROW_ON_ERROR);
+    }
+}
