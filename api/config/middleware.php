799c49916ed7c156e0f7e127834dae32864872dd
skazkin <russkazkin@gmail.com>
2024-06-16 06:43:57 +0300

    Add CleanEmptyInput middleware

--- api/config/middleware.php
+++ api/config/middleware.php
@@ -2,10 +2,12 @@
 
 declare(strict_types=1);
 
+use App\Http\Middleware\ClearEmptyInput;
 use Slim\App;
 use Slim\Middleware\ErrorMiddleware;
 
 return static function (App $app): void {
+    $app->add(ClearEmptyInput::class);
     $app->addBodyParsingMiddleware();
     $app->add(ErrorMiddleware::class);
 };
