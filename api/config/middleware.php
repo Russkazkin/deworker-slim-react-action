d2f9e7c3812c5807c77093d0ea0c070bd7af5e08
skazkin <russkazkin@gmail.com>
2024-06-22 09:01:41 +0300

    Extract validation handling

--- api/config/middleware.php
+++ api/config/middleware.php
@@ -2,12 +2,13 @@
 
 declare(strict_types=1);
 
-use App\Http\Middleware\{ClearEmptyInput, DomainExceptionHandler};
+use App\Http\Middleware\{ClearEmptyInput, DomainExceptionHandler, ValidationExceptionHandler};
 use Slim\App;
 use Slim\Middleware\ErrorMiddleware;
 
 return static function (App $app): void {
     $app->add(DomainExceptionHandler::class);
+    $app->add(ValidationExceptionHandler::class);
     $app->add(ClearEmptyInput::class);
     $app->addBodyParsingMiddleware();
     $app->add(ErrorMiddleware::class);
