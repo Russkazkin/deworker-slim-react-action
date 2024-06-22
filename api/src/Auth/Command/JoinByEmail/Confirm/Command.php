5b806355ae2da72c46ee7f7252fd440c87e15a38
skazkin <russkazkin@gmail.com>
2024-06-22 06:44:20 +0300

    Add validation rules

--- api/src/Auth/Command/JoinByEmail/Confirm/Command.php
+++ api/src/Auth/Command/JoinByEmail/Confirm/Command.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 namespace App\Auth\Command\JoinByEmail\Confirm;
 
+use Symfony\Component\Validator\Constraints\NotBlank;
+
 class Command
 {
+    #[NotBlank]
     public string $token = '';
 }
--- api/src/Auth/Command/JoinByEmail/Request/Command.php
+++ api/src/Auth/Command/JoinByEmail/Request/Command.php
@@ -4,8 +4,16 @@ declare(strict_types=1);
 
 namespace App\Auth\Command\JoinByEmail\Request;
 
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
 class Command
 {
+    #[NotBlank]
+    #[Email]
     public string $email = '';
+    #[NotBlank]
+    #[Length(min: 6)]
     public string $password = '';
 }
