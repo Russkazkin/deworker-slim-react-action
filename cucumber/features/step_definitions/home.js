452ce48301da6e54ca47f860e008330c1864c2ce
skazkin <russkazkin@gmail.com>
2024-08-03 10:25:01 +0300

    Extract page step

--- cucumber/features/step_definitions/home.js
+++ cucumber/features/step_definitions/home.js
@@ -1,9 +1,5 @@
-import { When, Then } from '@cucumber/cucumber';
+import { Then } from '@cucumber/cucumber';
 import { expect } from 'chai';
-
-When('I open {string} page', async function (uri) {
-  return await this.page.goto('http://gateway:8080' + uri);
-});
 
 Then('I see welcome block', async function () {
   await this.page.waitForSelector('[data-test=welcome]');
---
+++ cucumber/features/support/steps/page.js
@@ -0,0 +1,5 @@
+import { When } from '@cucumber/cucumber';
+
+When('I open {string} page', async function (uri) {
+  return await this.page.goto('http://gateway:8080' + uri);
+});
