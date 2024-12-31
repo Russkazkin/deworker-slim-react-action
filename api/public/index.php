7485d4612dffffec70ff8c7cd48af6fa22beb1aa
skazkin <russkazkin@gmail.com>
2024-12-31 11:43:05 +0300

    Update App creation logic

--- api/public/index.php
+++ api/public/index.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-use Psr\Container\ContainerInterface;
+use DI\Container;
 use Slim\App;
 
 use function Sentry\init;
@@ -15,7 +15,7 @@ if (getenv('SENTRY_DSN')) {
     init(['dsn' => getenv('SENTRY_DSN'), 'traces_sample_rate' => 1.0,]);
 }
 
-/** @var ContainerInterface $container */
+/** @var Container $container */
 $container = require __DIR__ . '/../config/container.php';
 
 /** @var App $app */
