8624d28f9868809475e84675d1a712c09bd90c92
skazkin <russkazkin@gmail.com>
2025-01-06 08:24:25 +0300

    Rename Welcome component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.sass';
-import Welcome from './components/Welcome';
+import Welcome from './Welcome/Welcome';
 const App: React.FC = () => {
   return (
     <div className="App">
