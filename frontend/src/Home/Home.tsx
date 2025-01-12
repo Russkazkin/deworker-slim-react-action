01572da9c49b05ab35184ac02e680373bcf29e22
skazkin <russkazkin@gmail.com>
2025-01-12 13:28:57 +0300

    Unify layout style

--- frontend/src/Home/Home.tsx
+++ frontend/src/Home/Home.tsx
@@ -4,14 +4,16 @@ import FeatureFlag, { FeaturesEnum } from '../FeatureToggle';
 
 const Home: React.FC = () => {
   return (
-    <div data-testid="home" className={styles.content}>
-      <h1>Auction</h1>
-      <FeatureFlag not name={FeaturesEnum.WeAreHere}>
-        <p>We will be here soon!</p>
-      </FeatureFlag>
-      <FeatureFlag name={FeaturesEnum.WeAreHere}>
-        <p>We are here</p>
-      </FeatureFlag>
+    <div data-testid="home" className={styles.layout}>
+      <div className={styles.content}>
+        <h1>Auction</h1>
+        <FeatureFlag not name={FeaturesEnum.WeAreHere}>
+          <p>We will be here soon!</p>
+        </FeatureFlag>
+        <FeatureFlag name={FeaturesEnum.WeAreHere}>
+          <p>We are here</p>
+        </FeatureFlag>
+      </div>
     </div>
   );
 };
