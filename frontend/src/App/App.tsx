2d887939f1993405e779b6cbf9fed822cc1fa88f
skazkin <russkazkin@gmail.com>
2025-01-18 07:07:01 +0300

    Add router

--- frontend/src/App/App.tsx
+++ frontend/src/App/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.sass';
 import Home from '../Home';
 import { FeaturesProvider } from '../FeatureToggle';
+import { BrowserRouter, Route, Routes } from 'react-router';
 
 type Props = {
   features: string[];
@@ -10,11 +11,16 @@ type Props = {
 const App: React.FC<Props> = ({ features }) => {
   return (
     <FeaturesProvider features={features}>
-      <div className="App" data-testid="App">
-        <div className="app">
-          <Home />
+      <BrowserRouter>
+        <div className="App" data-testid="App">
+          <div className="app">
+            <Routes>
+              <Route path="/" element={<Home />} />
+            </Routes>
+            <Home />
+          </div>
         </div>
-      </div>
+      </BrowserRouter>
     </FeaturesProvider>
   );
 };
