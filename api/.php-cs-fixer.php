ddb7f2ef2f203f241b96b002197b9c648adec11c
skazkin <russkazkin@gmail.com>
2025-02-09 07:26:10 +0300

    Remove useless structure

--- api/.php-cs-fixer.php
+++ api/.php-cs-fixer.php
@@ -32,4 +32,7 @@ return
             'strict_comparison' => true,
             'strict_param' => true,
             'multiline_whitespace_before_semicolons' => ['strategy' => 'no_multi_line'],
+            'no_superfluous_elseif' => true,
+            'no_useless_else' => true,
+            'no_useless_return' => true,
         ]);
