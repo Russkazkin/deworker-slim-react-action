c2b36956c9d6fe41dadce9e84993ebb55cbd69cc
skazkin <russkazkin@gmail.com>
2025-02-22 07:57:23 +0300

    Add OAuth entities

---
+++ api/src/OAuth/Test/Unit/Entity/RefreshTokenTest.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\OAuth\Test\Unit\Entity;
+
+use App\OAuth\Entity\AccessToken;
+use App\OAuth\Entity\RefreshToken;
+use DateTimeImmutable;
+use PHPUnit\Framework\MockObject\Exception;
+use PHPUnit\Framework\TestCase;
+use Ramsey\Uuid\Uuid;
+
+/**
+ * @internal
+ */
+final class RefreshTokenTest extends TestCase
+{
+    /**
+     * @throws Exception
+     */
+    public function testCreate(): void
+    {
+        $token = new RefreshToken();
+
+        $token->setIdentifier($identifier = Uuid::uuid4()->toString());
+        $token->setExpiryDateTime($expiryDateTime = new DateTimeImmutable());
+        $token->setAccessToken($accessToken = self::createStub(AccessToken::class));
+
+        self::assertSame($accessToken, $token->getAccessToken());
+        self::assertSame($identifier, $token->getIdentifier());
+        self::assertSame($expiryDateTime, $token->getExpiryDateTime());
+    }
+}
