2d54e268e7169ecd5685d9b3a26859f17b64df6e
skazkin <russkazkin@gmail.com>
2025-03-08 07:43:50 +0300

    Add authenticate middleware

---
+++ api/src/Http/Middleware/Authenticate.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Middleware;
+
+use League\OAuth2\Server\Exception\OAuthServerException;
+use League\OAuth2\Server\ResourceServer;
+use Psr\Http\Message\ResponseFactoryInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+final class Authenticate implements MiddlewareInterface
+{
+    public function __construct(private readonly ResourceServer $server, private readonly ResponseFactoryInterface $response) {}
+
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        try {
+            $request = $this->server->validateAuthenticatedRequest($request);
+        } catch (OAuthServerException $exception) {
+            return $exception->generateHttpResponse($this->response->createResponse());
+        }
+
+        return $handler->handle($request);
+    }
+}
