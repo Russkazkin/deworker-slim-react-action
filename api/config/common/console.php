5f231929ca2aae726f42370dd44bf9af9631d3ee
skazkin <russkazkin@gmail.com>
2025-03-15 13:10:36 +0300

    Add expired items clear command

--- api/config/common/console.php
+++ api/config/common/console.php
@@ -2,10 +2,13 @@
 
 declare(strict_types=1);
 
+use App\OAuth\Console\ClearExpiredCommand;
+
 return [
     'config' => [
         'console' => [
             'commands' => [
+                ClearExpiredCommand::class,
             ],
         ],
     ],
